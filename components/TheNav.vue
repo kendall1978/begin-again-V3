729c29a407f41f84a277628937843c28b3cc3e19
Cole Long <lolecong123@gmail.com>
2023-12-16 09:42:17 -0600

    Font Awesome changes, and fixing navbar

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,38 +1,57 @@
 <template>
-    <nav class="navbar is-fixed-top has-shadow" style="background: #fff;" id="my-nav" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a role="button" class="navbar-burger" id="burger" aria-label="menu" aria-expanded="false">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
+  <nav
+    class="navbar is-fixed-top has-shadow"
+    style="background: #fff"
+    id="my-nav"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <a
+        role="button"
+        class="navbar-burger"
+        id="burger"
+        aria-label="menu"
+        aria-expanded="false"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu" id="nav-links">
+      <div class="navbar-start">
+        <a class="navbar-item" href="">Megan's Place</a>
+        <a class="navbar-item" href="">Board of Directors</a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a
+              class="button is-facebook"
+              href="https://www.facebook.com/Begin-Again-Ministries-108864408483113"
+            >
+              <span class="icon">
+                <font-awesome-icon icon="fa-brands fa-facebook" />
+              </span>
+              <span>Facebook</span>
             </a>
+            <a
+              class="button is-dark"
+              href="https://www.paypal.com/US/fundraiser/charity/4542822"
+            >
+              <span class="icon">
+                <font-awesome-icon icon="fa-brands fa-paypal" />
+              </span>
+              <span>DONATE</span>
+            </a>
+          </div>
         </div>
-
-        <div class="navbar-menu" id="nav-links">
-            <div class="navbar-start">
-                <a class="navbar-item" href="">Megan's Place</a>
-                <a class="navbar-item" href="">Board of Directors</a>
-            </div>
-
-            <div class="navbar-end">
-            <div class="navbar-item">
-                <div class="buttons">
-                <a class="button is-facebook" href="https://www.facebook.com/Begin-Again-Ministries-108864408483113">
-                    <span class="icon">
-                        <i class="fab fa-facebook"></i>
-                    </span>
-                    <span>Facebook</span>
-                </a>
-                <a class="button is-dark" href="https://www.paypal.com/US/fundraiser/charity/4542822">
-                    DONATE
-                </a>
-                </div>
-            </div>
-            </div>
-        </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
 
-<script>
-
-</script>
+<script></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-    modules: [
-        '@pinia/nuxt'
-    ]
-})
+  modules: ["@pinia/nuxt", "@nuxt/fontawesome"],
+  css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+});
